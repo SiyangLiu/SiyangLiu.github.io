fc8a85688beb372725fd636353e1893331816c81
SiyangLiu <liusiyang.ocean@gmail.com>
2015-05-17 21:18:37 +0200

    first commit

---
+++ github.sh
@@ -0,0 +1,6 @@
+git init
+git config user.name SiyangLiu
+git config user.email liusiyang.ocean@gmail.com
+git remote add origin git@github.com:SiyangLiu/SiyangLiu.github.io.git
+git push -u origin master
+
