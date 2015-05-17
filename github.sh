28aacec750e75ef414c472fd3db8abd4efb16112
SiyangLiu <liusiyang.ocean@gmail.com>
2015-05-17 21:31:39 +0200

    Shujia's blog

--- github.sh
+++ github.sh
@@ -1,6 +1,14 @@
+#
 git init
 git config user.name SiyangLiu
 git config user.email liusiyang.ocean@gmail.com
 git remote add origin git@github.com:SiyangLiu/SiyangLiu.github.io.git
 git push -u origin master
-
+:<<'BLOCK'
+git pull #it will pull all the changes from the website to the local; when you have modified anything on the web, you must pull before you can push anything.
+touch index.html
+vi index.html #write Hello World
+git add index.html
+git commit -m "Index"
+git push
+BLOCK
